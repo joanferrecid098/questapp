4023d815908973095853c1363ec76bde99ec7772
joanferrecid098 <6hpd15h0-github@tigelcid.es>
2024-09-14 12:30:25 +0200

    feat(dev): create frontend structure

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Navbar from "./Navbar.svelte";
+    import "../app.css";
+</script>
+
+<div class="app">
+    <Navbar />
+
+    <main>
+        <slot />
+    </main>
+</div>
+
+<style>
+    .app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    main {
+        width: 100%;
+        box-sizing: border-box;
+        padding-left: 12rem;
+        padding-right: 4rem;
+        padding-top: 4rem;
+    }
+</style>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+
+<section>
+    <h1>QuestApp</h1>
+    <p>paragraph</p>
+</section>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        align-items: start;
+        align-content: stretch;
+        justify-content: start;
+        gap: 2rem;
+        width: 100%;
+        height: 100%;
+    }
+</style>
