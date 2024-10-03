c191b2ae5320162f2333f4088cb95eb75c71e316
joanferrecid098 <6hpd15h0-github@tigelcid.es>
2024-10-03 19:39:24 +0200

    feat(frontend): protect routes (temp)

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,11 +1,21 @@
 import { sessionStore } from "$stores/auth";
 import type { Handle } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
     const token = event.cookies.get("session");
 
+    const unprotectedRoutes = ["/login", "/register"];
+
     if (token) {
         sessionStore.set(token);
+    }
+
+    if (
+        (!token || token === "") &&
+        !unprotectedRoutes.some((route) => route === event.url.pathname)
+    ) {
+        throw redirect(302, "/login");
     }
 
     return await resolve(event);
